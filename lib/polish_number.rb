d4789412fa53d4e5ed0f0abfa45eac156d3f83a6
Wojciech Piekutowski <wojciech@piekutowski.net>
2010-06-10 00:26:12 +0200

    Refactoring. Numbers 0-999999 are supported. Raise exception when the number is out of range

--- lib/polish_number.rb
+++ lib/polish_number.rb
@@ -1,170 +1,69 @@
 # encoding: UTF-8
 
 module PolishNumber
-  TRANSLATIONS = {
-    0 => 'zero',
-    1 => 'jeden',
-    2 => 'dwa',
-    3 => 'trzy',
-    4 => 'cztery',
-    5 => 'pięć',
-    7 => 'siedem',
-    6 => 'sześć',
-    8 => 'osiem',
-    9 => 'dziewięć',
-    10 => 'dziesięć',
-    11 => 'jedenaście',
-    12 => 'dwanaście',
-    13 => 'trzynaście',
-    14 => 'czternaście',
-    15 => 'piętnaście',
-    16 => 'szesnaście',
-    17 => 'siedemnaście',
-    18 => 'osiemnaście',
-    19 => 'dziewiętnaście',
-    20 => 'dwadzieścia',
-    30 => 'trzydzieści',
-    40 => 'czterdzieści',
-    50 => 'pięćdziesiąt',
-    60 => 'sześćdziesiąt',
-    70 => 'siedemdziesiąt',
-    80 => 'osiemdziesiąt',
-    90 => 'dziewięćdziesiąt',
-    100 => 'sto',
-    200 => 'dwieście',
-    300 => 'trzysta',
-    400 => 'czterysta',
-    500 => 'pięćset',
-    600 => 'sześćset',
-    700 => 'siedemset',
-    800 => 'osiemset',
-    900 => 'dziewięćset',
-    1000 => 'tysiąc'
-  }.freeze
+  HUNDREDS = ['', 'sto ', 'dwieście ', 'trzysta ', 'czterysta ', 'pięćset ', 'sześćset ',
+    'siedemset ', 'osiemset ', 'dziewięćset ']
 
-  def self.rehash(hash)
-    result = Hash.new(hash['5'])
-    result[0] = hash['5']
-    result[1] = hash['1']
-    result[2] = result[3] = result[4] = hash['2_3_4']
-  end
+  TENS = ['', 'dziesięć ', 'dwadzieścia ', 'trzydzieści ', 'czterdzieści ', 'pięćdziesiąt ',
+    'sześćdziesiąt ', 'siedemdziesiąt ', 'osiemdziesiąt ', 'dziewięćdziesiąt ']
 
-  THOUSANDS = rehash({
-    '1' => 'tysiąc',
-    '2_3_4' => 'tysiące', # 22..24, 32..34
-    '5' => 'tysięcy', # 0, 5..21, 25..31
-  })
+  TEENS = ['', 'jedenaście ', 'dwanaście ', 'trzynaście ', 'czternaście ', 'piętnaście ',
+    'szesnaście ', 'siedemnaście ', 'osiemnaście ', 'dziewiętnaście ']
 
-  def self.translate_0_19(number)
-    TRANSLATIONS[number]
-  end
+  UNITIES = ['', 'jeden ', 'dwa ', 'trzy ', 'cztery ', 'pięć ', 'sześć ', 'siedem ', 'osiem ',
+    'dziewięć ']
 
-  def self.translate_20_99(number)
-    return TRANSLATIONS[number] if TRANSLATIONS.has_key? number
+  ZERO = 'zero'
 
-    first, second = number.to_s.split(//).map { |c| c.to_i }
-    "#{TRANSLATIONS[first*10]} #{TRANSLATIONS[second]}"
-  end
-
-  def self.translate_0_99(number)
-    number < 20 ? translate_0_19(number) : translate_20_99(number)
-  end
-
-  def self.translate_100_999(number)
-    return TRANSLATIONS[number] if TRANSLATIONS.has_key? number
-
-    hundreds = number/100
-    translations = []
-    translations << TRANSLATIONS[hundreds*100]
-
-    tens_units = number%100
-    if tens_units > 0
-      translations << translate_0_99(tens_units)
-    end
-
-    translations.join(' ')
-  end
-
-  def self.translate_0_999(number)
-    number < 100 ? translate_0_99(number) : translate_100_999(number)
-  end
-
-  def self.translate_1000_9999(number)
-    return TRANSLATIONS[number] if TRANSLATIONS.has_key? number
-
-    translations = []
-
-    thousands = number/1000
-    if thousands == 1
-      translations << TRANSLATIONS[1000]
-    else
-      translations << "#{translate_100_999(number)} #{pluralize(thousands, THOUSANDS)}"
-    end
-
-    number %= 1000
-    hundreds = number/100
-    translations << translate_0_999(number)
-
-    translations.join(' ')
-  end
+  THOUSANDS = ['', 'tysiąc ', 'tysiące ', 'tysięcy ']
 
   def self.translate(number)
     number = number.to_i
-    case number
-    when 0..19
-      return translate_0_19(number)
-    when 20..99
-      return translate_20_99(number)
-    when 100..999
-      return translate_100_999(number)
-    when 1000..9999
-      return translate_1000_9999(number)
+
+    unless (0..999999).include? number
+      raise ArgumentError, 'number should be in 0..999999 range'
     end
+
+    return ZERO.dup if number == 0
+
+    formatted_number = sprintf('%06.0f', number)
+    digits = formatted_number.chars.map { |char| char.to_i }
+
+    result = ''
+    result << process_0_999(digits[0..2])
+    result << thousands(digits[0..2])
+    result << process_0_999(digits[3..5])
+    result.strip!
+    result
   end
 
-  def self.translate_3_digit_number(number)
-    unless (0..999) === number
-      raise "#{number} not supported"
-    end
+  private
 
-    # handle simple numbers
-    if TRANSLATIONS.has_key? number
-      return TRANSLATIONS[number]
-    end
+  def self.process_0_999(digits)
+    result = ''
+    result << HUNDREDS[digits[0]]
 
-    number_digits = number.to_s.split(//)
-
-    last_2_digits =
-    if number > 99
-      number_digits[-2, 2]
+    if digits[1] == 1 && digits[2] != 0
+      result << TEENS[digits[2]]
     else
-      number_digits
+      result << TENS[digits[1]]
+      result << UNITIES[digits[2]]
     end
 
-    last_2_digits = last_2_digits.join.to_i
-    last_2_translation = TRANSLATIONS[last_2_digits]
+    result
+  end
 
-    translation_digits = []
-
-    if last_2_translation
-      translation_digits.unshift last_2_digits
-      number /= 100
-      position = 2
+  def self.thousands(digits)
+    if digits[0] == 0 && digits[1] == 0 && digits[2] == 0
+      thousand_id = 0
+    elsif digits[0] == 0 && digits[1] == 0 && digits[2] == 1
+      thousand_id = 1
+    elsif digits[1] != 1 && (2..4).include?(digits[2])
+      thousand_id = 2
     else
-      position = 0
+      thousand_id = 3
     end
-
-    while number != 0
-      digit = number%10
-      if digit > 0
-        power = 10**position
-        translation_digits.unshift digit*power
-      end
-      number /= 10
-      position += 1
-    end
-
-    translation_digits
+    THOUSANDS[thousand_id]
   end
 end
 
--- spec/polish_number_spec.rb
+++ spec/polish_number_spec.rb
@@ -20,15 +20,29 @@ describe :PolishNumber do
     323 => 'trzysta dwadzieścia trzy',
     440 => 'czterysta czterdzieści',
     999 => 'dziewięćset dziewięćdziesiąt dziewięć',
-    1000 => 'tysiąc',
-    1002 => 'tysiąc dwa',
-    1011 => 'tysiąc jedenaście',
-    1111 => 'tysiąc sto jedenaście',
-    1222 => 'tysiąc dwieście dwadzieścia dwa'
+    1000 => 'jeden tysiąc',
+    1002 => 'jeden tysiąc dwa',
+    1011 => 'jeden tysiąc jedenaście',
+    1111 => 'jeden tysiąc sto jedenaście',
+    1222 => 'jeden tysiąc dwieście dwadzieścia dwa',
+    2415 => 'dwa tysiące czterysta piętnaście',
+    5000 => 'pięć tysięcy',
+    10000 => 'dziesięć tysięcy',
+    22141 => 'dwadzieścia dwa tysiące sto czterdzieści jeden',
+    123754 => 'sto dwadzieścia trzy tysiące siedemset pięćdziesiąt cztery',
+    999999 => 'dziewięćset dziewięćdziesiąt dziewięć tysięcy dziewięćset dziewięćdziesiąt dziewięć'
   }.each do |number, translation|
     it "should translate #{number} to '#{translation}'" do
       PolishNumber.translate(number).should == translation
     end
+  end
+
+  it "should raise ArgumentError when number is smaller than 0" do
+    lambda { PolishNumber.translate(-1) }.should.raise(ArgumentError)
+  end
+
+  it "should raise ArgumentError when number is greater than 999999.99" do
+    lambda { PolishNumber.translate(1_000_000) }.should.raise(ArgumentError)
   end
 end
 
